727af877e9dc5298d5c451a07415037203caf0da
gkomsai <gkomsai788@gmail.com>
2022-10-06 21:08:54 +0530

    ProfilePage Templete created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Home from './pages/home/Home';
+// import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
 
 function App() {
   return (
     <div className="App">
      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
       <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
-      <Home/>
+      {/* <Home/> */}
+      <Profile />
     </div>
   );
 }
