343ca514636f3cf05ab650a78111b9e91ca77ee3
gkomsai <gkomsai788@gmail.com>
2022-10-06 13:39:28 +0530

    Profile card created

--- src/components/LeftSide/LeftSide.jsx
+++ src/components/LeftSide/LeftSide.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ProfileCard from '../ProfileCard/ProfileCard'
-import Searchbar from '../Searchbar/Searchbar'
-
+import React from "react";
+import ProfileCard from "../ProfileCard/ProfileCard";
+import Searchbar from "../Searchbar/Searchbar";
+import "./Leftside.css";
 const Leftside = () => {
   return (
-    <div>
-        <Searchbar />
-        <ProfileCard/>
+    <div className="leftside">
+      <Searchbar />
+      <ProfileCard />
     </div>
-  )
-}
+  );
+};
 
-export default Leftside
+export default Leftside;
--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-
 import cover from "../../assets/cover.jpg";
 import profile from "../../assets/profileImg.jpg";
+import "./profileCard.css";
+// import "../LeftSide/LeftSide.css";
+
 const ProfileCard = () => {
   return (
     <div className="profileCard">
@@ -9,8 +11,35 @@ const ProfileCard = () => {
         <img src={cover} alt="" />
         <img src={profile} alt="" />
       </div>
+      <div className="profileName">
+        <span>Diya</span>
+        <span>senior UI UX Designer</span>
+      </div>
+
+      <div className="followStatus">
+        <hr />
+        <div>
+          <div className="follow">
+            <span>6,890</span>
+            <span>Followers</span>
+          </div>
+          <div className="vl"> </div>
+          <div className="follow">
+            <span>2,084</span>
+            <span>Following</span>
+          </div>
+
+          <div className="vl"></div>
+          <div className="follow">
+            <span>1,436</span>
+            <span>Posts</span>
+          </div>
+        </div>
+        <hr />
+      </div>
+
+      <span>My Profile</span>
     </div>
   );
 };
-
 export default ProfileCard;
