2eadbdd667ac72e90513d4c0f5809f787b4ac2ce
gkomsai <gkomsai788@gmail.com>
2022-10-06 20:52:10 +0530

    Home page rightside section completed

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -4,17 +4,22 @@ import Home from "../../assets/home.png";
 import Notification from "../../assets/noti.png";
 import Comment from "../../assets/comment.png";
 import { UilSetting } from "@iconscout/react-unicons";
+import TrendCard from "../TrendCard/TrendCard";
 
 const RightSide = () => {
   return (
     <div className="RightSide">
       <div className="navIcons">
         <img src={Home} alt="" />
-
         <UilSetting />
         <img src={Notification} alt="" />
-
         <img src={Comment} alt="" />
+      </div>
+      <TrendCard />
+      <div>
+        <button className="button right-share-btn">
+          Share
+        </button>
       </div>
     </div>
   );
