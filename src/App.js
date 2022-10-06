df8559eaa49439ecd90c0919ccc25dd0bc994264
gkomsai <gkomsai788@gmail.com>
2022-10-06 10:14:21 +0530

    All root variables defined

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+     <div className="blur" style={{ top: "-18%", right: "0" }}></div>
+      <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
     </div>
   );
 }
