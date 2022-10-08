bcc793d45c3205bd5ccd9ee143c513b86287e54b
gkomsai <gkomsai788@gmail.com>
2022-10-08 06:28:40 +0530

    Profile page left side completed

--- src/components/ProfilePageLeft/ProfileLeft.jsx
+++ src/components/ProfilePageLeft/ProfileLeft.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import FollowersCard from "../FollowersCard/FollowersCard";
+import InfoCard from "../InfoCard/InfoCard";
+import Searchbar from "../Searchbar/Searchbar";
+import "../LeftSide/Leftside.css"
+import '../FollowersCard/FollowersCard.css'
 const ProfileLeft = () => {
   return (
-    <div>ProfileLeft</div>
-  )
-}
+    <div className="leftside">
+      <Searchbar />
+      <InfoCard />
+      <FollowersCard />
+    </div>
+  );
+};
 
-export default ProfileLeft
+export default ProfileLeft;
