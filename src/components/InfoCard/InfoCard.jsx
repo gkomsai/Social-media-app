4ba4491012a0d22441d2055dd85ea5c6a4602028
gkomsai <gkomsai788@gmail.com>
2022-10-08 14:54:28 +0530

    Share model and Profile model completed

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -1,27 +1,26 @@
 import React from "react";
-import { useState } from "react";
+
 import ProfileModal from "../ProfileModal/ProfileModal";
 import "./InfoCard.css";
 import { UilPen } from "@iconscout/react-unicons";
+import { useDisclosure } from "@chakra-ui/react";
+
 const InfoCard = () => {
-  const [modalOpened, setModalOpened] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <div className="InfoCard">
       <div className="infoHead">
         <h4>Profile Info</h4>
         <div>
-            <UilPen
-              width="2rem"
-              height="1.2rem"
-              onClick={() => setModalOpened(true)}
-            />
-            <ProfileModal
-              modalOpened={modalOpened}
-              setModalOpened={setModalOpened}
-            
-            />
-          </div>
+          <UilPen
+            width="2rem"
+            height="1.2rem"
+            onClick={onOpen}
+            backgroundColor="#24be6a"
+          />
+          <ProfileModal isOpen={isOpen} onClose={onClose} />
+        </div>
       </div>
 
       <div className="info">
