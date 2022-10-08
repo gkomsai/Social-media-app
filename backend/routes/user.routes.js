970716ef1ceda4d0cc8cc5a887899862bed3bc04
gkomsai <gkomsai788@gmail.com>
2022-10-08 18:07:43 +0530

    update user logic written

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,41 +1,76 @@
-const {Router} = require ("express");
+const { Router } = require("express");
 const userRouter = Router();
 
 /*  ----------------------for getting a singleuser-------------------------------- */
 
-userRouter.get('/:id',  async (req, res) => {
-    const id = req.params.id;
-    try {
-      const user = await UserModel.findById(id);
-      if (user) {
-        const { password, ...otherDetails } = user._doc; // note- all the details we are receiving in the response  will lie under this ._doc
-        res.status(200).send(otherDetails);
-      } else {
-        return res
+userRouter.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await UserModel.findById(id);
+    if (user) {
+      const { password, ...otherDetails } = user._doc; // note- all the details we are receiving in the response  will lie under this ._doc
+      res.status(200).send(otherDetails);
+    } else {
+      return res
         .status(400)
         .send({ status: "error", message: "No such user exists" });
-      }
-    } catch (error) {
-      res.status(500).json(error);
     }
-  });
+  } catch (err) {
+    res.status(500).send({ status: "error", message: err.message });
+  }
+});
 
+/*  ----------------------for getting a allusers-------------------------------- */
+userRouter.get("/", async (req, res) => {
+  try {
+    let users = await UserModel.find();
+    users = users.map((user) => {
+      const { password, ...otherDetails } = user._doc;
+      return otherDetails;
+    });
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).send({ status: "error", message: err.message });
+  }
+});
 
+/*  ----------------------for updating  a user-------------------------------- */
 
-userRouter.get('/',async (req, res) => {
+userRouter.patch("/id", async (req, res) => {
+  const id = req.params.id; // the id of the person who wants to update
+  
+  const { _id, currentUserAdmin, password } = req.body; 
+
+  if (id === _id) { 
     try {
-      let users = await UserModel.find();
-      users = users.map((user)=>{
-        const {password, ...otherDetails} = user._doc
-        return otherDetails
-      })
-      res.status(200).json(users);
-    } catch (error) {
-      res.status(500).json(error);
+      // if we also have to update password then password will be bcrypted again
+      if (password) {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(password, salt);
+      }
+
+      const user = await UserModel.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      const token = jwt.sign(
+        { username: user.username, id: user._id },
+        process.env.JWT_SECRET_KEY,
+        { expiresIn: "2h" }
+      );
+      console.log({ user, token });
+      return res.status(200).send({ user, token });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({ status: "error", message: err.message });
     }
-  });
+  } else {
+    return res
+      .status(403)
+      .send({
+        status: "error",
+        message: "Access Denied! You can update only your own Account.",
+      });
+  }
+});
 
-
-
-
-module.exports = {userRouter}
+module.exports = { userRouter };
