7f4a848b9becde5e3bcdf9842ec3b28e1658c3d9
gkomsai <gkomsai788@gmail.com>
2022-10-08 13:02:54 +0530

    Profile modal component created

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import { useState } from "react";
+import ProfileModal from "../ProfileModal/ProfileModal";
 import "./InfoCard.css";
-
+import { UilPen } from "@iconscout/react-unicons";
 const InfoCard = () => {
+  const [modalOpened, setModalOpened] = useState(false);
+
   return (
     <div className="InfoCard">
       <div className="infoHead">
         <h4>Profile Info</h4>
-        <div></div>
+        <div>
+            <UilPen
+              width="2rem"
+              height="1.2rem"
+              onClick={() => setModalOpened(true)}
+            />
+            <ProfileModal
+              modalOpened={modalOpened}
+              setModalOpened={setModalOpened}
+            
+            />
+          </div>
       </div>
 
       <div className="info">
