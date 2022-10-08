83769f140c49a203afaa66fa93a80769ee54565e
gkomsai <gkomsai788@gmail.com>
2022-10-08 17:06:14 +0530

    signup implemented successfully

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const { connection } = require("./config/db.js");
+const { userRouter } = require("./routes/user.routes.js");
+
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to social-media-app backend server home page");
+});
+
+app.listen(port, async () => {
+  try {
+    await connection;
+    console.log(`app is listening on port ${port}`);
+    console.log("conneted to DB");
+  } catch (error) {
+    console.log(error);
+  }
+});
---
+++ backend/models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    profilePicture: String,
+    coverPicture: String,
+    about: String,
+    livesIn: String,
+    worksAt: String,
+    relationship: String,
+    country: String,
+    followers: [],
+    following: [],
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const UserModel = mongoose.model("user", UserSchema);
+module.exports = { UserModel };
