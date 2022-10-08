6bfc38a2a68a1dd30a98aee32e7b0cceb081ee05
gkomsai <gkomsai788@gmail.com>
2022-10-08 11:06:35 +0530

    profile page right side completed

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import MiddleSide from "../../components/MiddleSide/MiddleSide";
 import ProfileCard from "../../components/ProfileCard/ProfileCard";
 import ProfileLeft from "../../components/ProfilePageLeft/ProfileLeft";
+import RightSide from "../../components/RightSide/RightSide";
 import "./profile.css";
 
 const Profile = () => {
@@ -9,9 +10,10 @@ const Profile = () => {
     <div className="Profile">
       <ProfileLeft />
       <div className="profile-centre">
-        <ProfileCard/>
-        <MiddleSide/>
+        <ProfileCard />
+        <MiddleSide />
       </div>
+      <RightSide />
     </div>
   );
 };
