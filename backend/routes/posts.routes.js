62c54713c9c27dd5fd77d8824602485e2201c9e2
gkomsai <gkomsai788@gmail.com>
2022-10-08 20:51:14 +0530

    for getting all a single post and all the post logic created

--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -16,5 +16,37 @@ postsRouter.post("/create", async (req, res) => {
   }
 });
 
+ /*  ----------------------for getting a post-------------------------------- */
+postsRouter.post("/:id", async (req, res) => {
+    const id = req.params.id;
+  
+    try {
+      const foundPost = await PostModel.findById(id);
+      res.status(200).send(foundPost);
+    } catch (err) {
+        return res.status(500).send({ status: "error", message: err.message });
+    };
+  });
+
+
+   /*  ----------------------for getting all the posts of a particular user-------------------------------- */
+postsRouter.post("/", async (req, res) => {
+    const userId = req.body.userId;
+    try {
+      const foundPost = await PostModel.find({userId});
+
+      if (foundPost.length > 0) {
+        res.status(200).send(foundPost);
+      } else {
+        return res.send({ message: "First create a Post" });
+      }
+    } catch (err) {
+        return res.status(500).send({ status: "error", message: err.message });
+    };
+  });
+
+  
+
+
 
 module.exports = {postsRouter};
