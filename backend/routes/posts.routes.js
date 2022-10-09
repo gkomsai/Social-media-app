2f0ac3744bba61de59f21a721e944beec9ad4c85
gkomsai <gkomsai788@gmail.com>
2022-10-09 06:24:36 +0530

    patch and delete request added for posts

--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,6 +1,11 @@
 const { Router } = require("express");
+const { checkUserAuth } = require("../middleware/authMiddleware");
 const { PostModel } = require("../models/postModel");
 const postsRouter = Router();
+
+
+
+postsRouter.use(checkUserAuth);
 
 
 /*  ----------------------for creating a new post-------------------------------- */
@@ -43,6 +48,46 @@ postsRouter.post("/", async (req, res) => {
     } catch (err) {
         return res.status(500).send({ status: "error", message: err.message });
     };
+  });
+
+  /*  ----------------------for updating a posts of a particular user-------------------------------- */
+  
+postsRouter.patch("/:id", async (req, res) => {
+    const postId = req.params.id;
+    const { userId } = req.body;
+  
+    try {
+      const post = await PostModel.findById(postId);
+      if (post.userId === userId) {
+        await post.updateOne({ $set: req.body });
+        res.status(200).send({ status: "success", message: "Post updated Successfully!" });
+      } else {
+        res.status(403).send({ status: "error", message: "Authentication failed" }) ;
+      }
+    }catch (err) {
+        return res.status(500).send({ status: "error", message: err.message });
+    }
+  });
+
+
+
+  /*  ----------------------for getting all the posts of a particular user-------------------------------- */
+
+  postsRouter.delete("/:id", async (req, res) => {
+    const id = req.params.id;
+    const { userId } = req.body;
+  
+    try {
+      const post = await PostModel.findById(id);
+      if (post.userId === userId) {
+        await post.deleteOne();
+       return res.status(200).send({ status: "success", message: "Post deleted Successfully!" });
+      } else {
+      return  res.status(403).send({ status: "error", message:"Action forbidden" });
+      }
+    } catch (error) {
+        return res.status(500).send({ status: "error", message: err.message });
+    }
   });
 
   
