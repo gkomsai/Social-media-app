53c16dbddd247caab06f2fc1b3fff83d0b6e8b48
gkomsai <gkomsai788@gmail.com>
2022-10-10 09:57:38 +0530

    Auth component updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,8 +9,8 @@ function App() {
      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
       <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
       {/* <Home/> */}
-      <Profile />
-      {/* <Auth /> */}
+      {/* <Profile /> */}
+      <Auth />
     </div>
   );
 }
