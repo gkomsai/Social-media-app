067b3132e1fb6b8bdc7599637b943f0bb010b16b
gkomsai <gkomsai788@gmail.com>
2022-11-01 16:25:53 +0530

    correctly rendered the react-input emoji in the UI

--- frontend/src/components/ChatBox/ChatBox.jsx
+++ frontend/src/components/ChatBox/ChatBox.jsx
@@ -6,10 +6,12 @@ import defaultProfile from "../../assets/defaultProfile.png";
 import "./ChatBox.css";
 import { format } from "timeago.js";
 import { getMessages } from "../../api/messageApi";
+import InputEmoji from "react-input-emoji";
 
 const ChatBox = ({ chat, currentUser }) => {
   const [userData, setUserData] = useState(null);
   const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
   console.log({ userData });
 
   const userId = chat?.members.find((id) => id !== currentUser);
@@ -31,7 +33,7 @@ const ChatBox = ({ chat, currentUser }) => {
     }
   }, [userId]);
 
-  // fetch messages
+  //for fetching messages
   useEffect(() => {
     const fetchMessages = async () => {
       try {
@@ -42,61 +44,74 @@ const ChatBox = ({ chat, currentUser }) => {
         console.log(error);
       }
     };
-
     if (chat) {
       fetchMessages();
     }
   }, [chat]);
 
 
-
+  const handleSend = async (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="ChatBox-container">
-      <div className="chat-header">
-        <div className="follower">
-          <div>
-            <img
-              src={
-                userData?.profilePicture
-                  ? userData.profilePicture
-                  : defaultProfile
-              }
-              alt="Profile"
-              className="followerImage"
-              style={{ width: "50px", height: "50px" }}
+      {chat ? (
+        <>
+          <div className="chat-header">
+            <div className="follower">
+              <div>
+                <img
+                  src={
+                    userData?.profilePicture
+                      ? userData.profilePicture
+                      : defaultProfile
+                  }
+                  alt="Profile"
+                  className="followerImage"
+                  style={{ width: "50px", height: "50px" }}
+                />
+                <div className="name" style={{ fontSize: "0.9rem" }}>
+                  <span>
+                    {userData?.firstName} {userData?.lastName}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <hr
+              style={{
+                width: "95%",
+                border: "0.1px solid #ececec",
+                marginTop: "20px",
+              }}
             />
-            <div className="name" style={{ fontSize: "0.9rem" }}>
-              <span>
-                {userData?.firstName} {userData?.lastName}
-              </span>
+          </div>
+          <div className="chat-body">
+            {messages.map((message, i) => (
+              <div
+                key={i}
+                className={
+                  message.senderId === currentUser ? "message own" : "message"
+                }
+              >
+                <span>{message.text}</span>{" "}
+                <span>{format(message.createdAt)}</span>
+              </div>
+            ))}
+          </div>
+          <div className="chat-sender">
+            <div>+</div>
+            <InputEmoji value={newMessage} onChange={setNewMessage} />
+            <div className="send-button button" onClick={handleSend}>
+              Send
             </div>
           </div>
-        </div>
-        <hr
-          style={{
-            width: "95%",
-            border: "0.1px solid #ececec",
-            marginTop: "20px",
-          }}
-        />
-      </div>
-
-      <div className="chat-body">
-        {messages.map((message,i) => (
- 
-            <div
-            key={i}
-              className={
-                message.senderId === currentUser ? "message own" : "message"
-              }
-            >
-              <span>{message.text}</span>{" "}
-              <span>{format(message.createdAt)}</span>
-            </div>
-  
-        ))}
-      </div>
+        </>
+      ) : (
+        <span className="chatbox-empty-message">
+          Tap on a chat to start conversation...
+        </span>
+      )}
     </div>
   );
 };
