d12fc881b77bde9a48c076bcf7536a0b42c06a4d
gkomsai <gkomsai788@gmail.com>
2022-11-11 14:51:23 +0530

    Home Page made responsive for  all screen sizes

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,15 +1,34 @@
+import { Grid, Hide, Show } from "@chakra-ui/react";
 import React from "react";
 import Leftside from "../../components/LeftSide/LeftSide";
 import MiddleSide from "../../components/MiddleSide/MiddleSide";
+import NavIcons from "../../components/NavIcons/Navicons";
 import RightSide from "../../components/RightSide/RightSide";
-import "./home.css";
+
 const Home = () => {
   return (
-    <div className="home">
-      <Leftside />
-      <MiddleSide />
-      <RightSide />
-    </div>
+    <>
+      <Show w="80%" m="auto" below="xl">
+        <NavIcons />
+      </Show>
+
+      <Grid
+        templateColumns={{
+          base: "1",
+          md: "24rem auto",
+          lg: "24rem auto",
+          xl: "22rem auto 24rem",
+        }}
+        position={"relative"}
+        gap={6}
+      >
+        <Leftside />
+        <MiddleSide />
+        <Hide below="xl">
+          <RightSide />
+        </Hide>
+      </Grid>
+    </>
   );
 };
 
