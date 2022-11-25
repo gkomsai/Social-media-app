c17dc2304ab2e8ea14c227c5d68df04b56d51c88
gkomsai <gkomsai788@gmail.com>
2022-11-26 01:26:10 +0530

    applied regex inplace of custom logic to check the email-password validation

--- backend/middleware/emailValidator.js
+++ backend/middleware/emailValidator.js
@@ -1,18 +1,17 @@
 const emailValidator = (req, res, next) => {
   if (req.method === "POST") {
     let email = req.body.email;
-
-    let end = "";
-    for (let i = email.length - 10; i < email.length; i++) {
-      end = end + email[i];
-    }
-
-    if (end === "@gmail.com") {
+    const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.match(mailformat)) {
       next();
     } else {
       return res
         .status(400)
-        .send({ status: "error", message: "Please enter valid email" });
+        .send({
+          status: "error",
+          message: "Please enter valid email",
+          description: null,
+        });
     }
   } else {
     next();
--- backend/middleware/passwordValidator.js
+++ backend/middleware/passwordValidator.js
@@ -1,53 +1,56 @@
 const passwordValidator = (req, res, next) => {
   if (req.method === "POST") {
     let { password } = req.body;
-    if(password){
-      let length = password.length;
-      let isnumber = false;
-  
-      for (let i = 0; i < length; i++) {
-        if (
-          password[i] == 0 ||
-          password[i] == 1 ||
-          password[i] == 2 ||
-          password[i] == 3 ||
-          password[i] == 4 ||
-          password[i] == 5 ||
-          password[i] == 6 ||
-          password[i] == 7 ||
-          password[i] == 8 ||
-          password[i] == 9
-        ) {
-          isnumber = true;
-          break;
-        }
-      }
-  
-      let specialChar = false;
-  
-      if (
-        password.includes("@") ||
-        password.includes("#") ||
-        password.includes("$") ||
-        password.includes("%") ||
-        password.includes("&") ||
-        password.includes("*")
-      ) {
-        specialChar = true;
-      }
-      if (isnumber && length > 7 && specialChar) {
+    if (password) {
+      const strongPassword =
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+      if (password.match(strongPassword)) {
+
+    /*   ---------------Custom Logic---------------------------  */
+        // let length = password.length;
+        // let isnumber = false;
+
+        // for (let i = 0; i < length; i++) {
+        //   if (
+        //     password[i] == 0 ||
+        //     password[i] == 1 ||
+        //     password[i] == 2 ||
+        //     password[i] == 3 ||
+        //     password[i] == 4 ||
+        //     password[i] == 5 ||
+        //     password[i] == 6 ||
+        //     password[i] == 7 ||
+        //     password[i] == 8 ||
+        //     password[i] == 9
+        //   ) {
+        //     isnumber = true;
+        //     break;
+        //   }
+        // }
+
+        // let specialChar = false;
+
+        // if (
+        //   password.includes("@") ||
+        //   password.includes("#") ||
+        //   password.includes("$") ||
+        //   password.includes("%") ||
+        //   password.includes("&") ||
+        //   password.includes("*")
+        // ) {
+        //   specialChar = true;
+        // }
+        // if (isnumber && length > 7 && specialChar) {
         next();
       } else {
-        return res
-          .status(400)
-          .send({
-            status: "error",
-            message:
-              "Password must be greater than 7 and must includes a number and specialCharater",
-          });
+        return res.status(400).send({
+          status: "error",
+          message: "Weak Password",
+          description:
+            "Password must be between 8 to 15 characters and contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character",
+        });
       }
     }
-   
   } else {
     next();
   }
